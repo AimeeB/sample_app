98c462acdf8fadbc587eb19d23182e1c9cda028c
Aimee Barciauskas <barciaai@gmail.com>
2012-01-05 14:08:57 -0500

    added about and contact pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,19 @@
 require 'spec_helper'
 
-describe PagesController do
+describe PagesController do  
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+  end
+  
+describe "test for right title" do
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => " | Home")
     end
   end
 
@@ -15,5 +23,26 @@ describe PagesController do
       response.should be_success
     end
   end
-
+  
+  describe "test for right title" do
+      it "should have the right title" do
+        get 'contact'
+        response.should have_selector("title", :content => " | Contact")
+      end
+    end
+    
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
+  
+  describe "test for right title" do
+      it "should have the right title" do
+        get 'about'
+        response.should have_selector("title", :content => " | About")
+      end
+    end
+  
 end
