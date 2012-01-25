f026a4031979e9bde0e24670c3db6ea6d83632aa
Aimee Barciauskas <barciaai@gmail.com>
2012-01-25 18:55:21 -0500

    hoping this works

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -6,7 +6,7 @@ class MicropostsController < ApplicationController
     @micropost = current_user.microposts.build(params[:micropost])
     if @micropost.save
       flash[:success] = "Micropost created!"
-      redirect_to root_path
+      redirect_to root_pathv
     else
       @feed_items = []
       render 'pages/home'
